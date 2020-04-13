a5d2df1f5cd58e798046b672ec1e49f77a9963d7
Chau Tran <chautran.tai@gmail.com>
2020-04-13 01:56:22 -0400

    readded client

---
+++ client/src/components/Game/Game.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Game = () => {
+    return (
+        <div>
+            <Container>
+                <Row>
+
+                </Row>
+            </Container>
+        </div>
+    );
+}
+
+export default Game;
