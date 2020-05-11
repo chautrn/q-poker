12ccbb968c15dbb2a6060c9bd717729a5245b91b
Chau Tran <chautran.tai@gmail.com>
2020-05-11 03:38:25 -0400

    Added helper functions to game.js

--- server/game.js
+++ server/game.js
@@ -1,5 +1,31 @@
 // Input are passed in from room-manager.js
 
+const createUser = (userId, username, balance) => {
+    return {
+        id: userId,
+        name: username,
+        currentBalance: balance
+    };
+}
+
+const createDeck = () => {
+    return [
+        '2s', '2c', '2h', '2d',
+        '3s', '3c', '3h', '3d',
+        '4s', '4c', '4h', '4d',
+        '5s', '5c', '5h', '5d',
+        '6s', '6c', '6h', '6d',
+        '7s', '7c', '7h', '7d',
+        '8s', '8c', '8h', '8d',
+        '9s', '9c', '9h', '9d',
+        'Ts', 'Tc', 'Th', 'Td',
+        'Js', 'Jc', 'Jh', 'Jd',
+        'Qs', 'Qc', 'Qh', 'Qd',
+        'Ks', 'Kc', 'Kh', 'Kd',
+        'As', 'Ac', 'Ah', 'Ad'
+    ];
+}
+
 class Poker {
     constructor(initialUser, { startingChips, timeLimit, punishment }) { // gameOptions = { startingChips, timeLimit, punishment }
         // Options
@@ -7,6 +33,9 @@ class Poker {
         this.timeLimit = timeLimit;
         this.punishment = punishment;
 
+        // Game data 
+        this.deck = createDeck();
+        this.users = [createUser(initialUser)];
         /*
             Betting Rounds:
             Preflop 0
@@ -14,10 +43,14 @@ class Poker {
             Turn 2
             River 3
             Showdown 4
-        */
-        this.pokerState = 0;
+            */
+        this.bettingRound = 0;
         this.pot = 0;
+        this.communityCards = [];
         this.buttonIndex = 0;
+
+        // Round data
+        this.currentBet;
     }
 }
 
