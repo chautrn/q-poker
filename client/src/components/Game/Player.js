8a9db29e924d2b0e6d9624925373d7f46cf66848
Chau Tran <chautran.tai@gmail.com>
2020-06-08 01:57:14 -0400

    Began player component

---
+++ client/src/components/Game/Player.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import styles from './Player.module.css';
+
+const Player = () => {
+    return (
+        <div className={styles['player-container']}>
+        </div>
+    )
+}
+
+export default Player;
