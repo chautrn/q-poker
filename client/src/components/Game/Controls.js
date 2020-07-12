f6beb51e8150f30c1aa01aeb78edea195ed0d3e1
chautrn <chautran.tai@gmail.com>
2020-07-12 00:32:50 -0400

    General UI Update
    - Redesigned player profile component on game board.
    - Styled slider in controls component.

--- client/src/components/Game/Controls.js
+++ client/src/components/Game/Controls.js
@@ -1,37 +1,71 @@
 import React, { useState } from 'react';
 import { Slider } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles'
 import styles from './Controls.module.css';
+
+const StyledSlider = withStyles({
+	rail: { 
+		backgroundColor: '#14CC60',
+		height: '10px',
+	},
+	track: {
+		backgroundColor: '#14CC60',
+		height: '10px',
+	},
+	thumb: {
+		height: 20,
+		width: 20,
+		backgroundColor: '#14CC60',
+		'&:hover, &:$active': {
+			boxShadow: '0 0 0 10px rgba(20, 204, 96, 0.2)'
+		},
+	},
+})(Slider);
 
 const Controls = () => {
 	const [raising, setRaising] = useState(false);
 
 	return (
 		<div id={styles['control-wrap']}>
-			<div id={styles['button-wrap']}>
-				<span 
-					className={styles['button']}
-					style={{
-						background: "#C75138",
-						borderBottom: "solid 4px #9C402C"
-					}}> FOLD </span>
-				<span className={styles['button']}> CHECK </span>
-				<div id={styles['raise-wrap']}>
-					<span 
-						className={styles['button']}
-						style={{
-							background: "#E3D900",
-							borderBottom: "solid 4px #9C9500",
-							marginRight: 0
-						}}
-						onClick={() => setRaising(!raising)}>
-						{raising ? 'CONFIRM' : 'RAISE'}
-					</span>
-					<div id={styles['raise-slider-wrap']}>
+			{raising 
+				? 
+					<div id={styles['slider-wrap']}>
+						<StyledSlider 
+							defaultValue={0}
+							step={215}
+							min={0}
+							max={3000}
+							disabled={false}
+						/>
 					</div>
-				</div>
-			</div>
+					:
+					<div id={styles['button-wrap']}>
+						<span 
+							className={styles['button']}
+							style={{
+								background: "#C75138",
+								borderBottom: "solid 4px #9C402C"}}> 
+							FOLD
+						</span>
+						<span className={styles['button']}> CHECK </span>
+						<div id={styles['raise-wrap']}>
+							<span 
+								className={styles['button']}
+								style={{
+									background: "#E3D900",
+									borderBottom: "solid 4px #9C9500",
+									marginRight: 0
+								}}
+								onClick={() => setRaising(!raising)}>
+								RAISE
+							</span>
+							<div id={styles['raise-slider-wrap']}>
+							</div>
+						</div>
+					</div>
+			}
 		</div>
-		);
+	);
 }
 
 export default Controls;
--- client/src/components/Game/Player.js
+++ client/src/components/Game/Player.js
@@ -3,22 +3,20 @@ import React from 'react';
 import styles from './Player.module.css';
 
 const Player = ({ position }) => {
-    return (
-        <div className={styles['player-container']} id={styles[`${position}`]}>
-                <div className={styles['profile-container']}>
-                    <div className={styles['name']}>
-                    </div>
-                    <div className={styles['picture']}>
-                    </div>
-                </div>
-                <div className={styles['hand-container']}>
-                </div>
-            <div className={styles['bet-container']}>
-            </div>
-            <div className={styles['chips-container']}>
-            </div>
-        </div>
-    );
+	return (
+		<div className={styles['player-container']} id={styles[`${position}`]}>
+			<div className={styles['name-container']}>
+				TheAwesomeChau	
+			</div>
+			<div className={styles['card-container']}>
+				<img src='/images/QH.png' />
+				<img src='/images/KS.png' />
+			</div>
+			<div className={styles['balance-container']}>
+				$1000
+			</div>
+		</div>
+	);
 }
 
 export default Player;
